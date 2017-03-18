017b8ffed3c120e04ca6e028e32120464aa39593
Martin Nogueras <jnogueras3@email.suagm.edu>
2017-03-18 14:41:12 -0400

    Add files via upload

---
+++ Quantu.py
@@ -0,0 +1,14 @@
+#Quantu 
+#The Qu programming language compiler
+#written in python firts and then in
+#other programing languages with 
+#a plug-in interface or an API of some
+#sort. Takes machine code generated in processor, 
+# and in the computer memory  and  passes to 
+#Quboid ready data structure.
+
+#To-do:
+#Start writing source-code
+
+def main():
+
